eab5a40c9878d5c780e305b896617d0063722910
Varun <valivetiavrun@gmail.com>
2025-02-09 01:32:32 -0800

    Yeah kinda got a better start, badgerdb working now

---
+++ blockchain/proof.go
@@ -0,0 +1,102 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+// To make the proof, we will take the data from the block, and create
+// a counter (formally known as the nonce) which starts at 0.
+// we will create hash + the nonce value, see if that matches the
+// required set of requirements. If it doesn't we will change the nonce value
+
+// Requirements:
+// First few bytes must contains 0s
+
+const Difficulty = 15
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
+
+	nonce := 0
+
+	for nonce < math.MaxInt64 {
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+
+		fmt.Printf("\r%x", hash)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+
+	fmt.Println()
+
+	return nonce, hash[:]
+
+}
+
+// Validate is like pretty much saying
+// Ok, so you validated the block with some nonce value
+// Let me just confirm that it's valid and it's actually
+// Meeting the requirements so it just runs the algo with that nonce
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := pow.InitData(pow.Block.Nonce)
+
+	hash := sha256.Sum256(data)
+
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/varunvaliveti/P2PBlockchain
+
+go 1.23.2
+
+require github.com/dgraph-io/badger v1.5.4
+
+require (
+	github.com/AndreasBriese/bbloom v0.0.0-20190825152654-46b345b51c96 // indirect
+	github.com/dgryski/go-farm v0.0.0-20240924180020-3414d57e47da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
